ea9dbfa4ad07d0fd93ed18964700dae6f64b05cd
Dustin <defender2k>
2021-07-03 22:03:29 -0500

    cleaning up context

--- game.js
+++ game.js
@@ -15,11 +15,13 @@ window.onload = function(){
 function tick(time){
 	if(Game.Cards.filter(x => !x.loaded).length == 0){
 		context.clearRect(0,0,canvas.width,canvas.height);
+		context.save();
 		context.translate(50,50);
 		for(let i in Game.Cards){
 			Game.Cards[i].draw(context);
 			context.translate(300,0);
 		}
+		context.restore();
 	}
 	window.requestAnimationFrame(tick);
 }
